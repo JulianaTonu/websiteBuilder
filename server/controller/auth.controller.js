62721b157975321d7cf703790ab838d89f5debd2
julianatonu <julianatonu@gmail.com>
2026-09-01 12:02:29 +0600

    Implement Google auth with JWT and logout

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,18 +1,39 @@
 import User from "../models/user.model"
 import jwt from "jsonwebtoken"
+
 export const googleAuth = async (req, res) => {
-try{
-    const { name, email, avatar } = req.body;
-    if (!email) {
-        return res.status(400).json({ message: 'Email is required' });
+    try {
+        const { name, email, avatar } = req.body;
+        if (!email) {
+            return res.status(400).json({ message: 'Email is required' });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            user = await User.create({ name, email, avatar });
+        }
+        const token = await jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        })
+        return res.status(200).json(user)
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message:`google auth error ${error}` });
     }
-    const user = await User.findOne({ email });
-    if (!user) {
-        user = await User.create({ name, email, avatar });
-    }
-    const token = await jwt.sign({id:user.id}, process.env.JWT_SECRET,{expiresIn: '7d'});
-}catch(error){
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
 }
+
+export const logOut = async (req, res)=>{
+    try {
+        return res.clearCookie('token',{
+            httpOnly: true,
+            secure: false,
+            sameSite: 'strict',
+        })
+    } catch (error) {
+        return res.status(500).json({ message:`Logout error ${error}`})
+    }
 }
